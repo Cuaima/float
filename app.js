03c837e22e38de2e8d90dcc252404b4dd05830b9
Cuaima <cl@udia.me>
2021-07-30 19:13:14 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  const lineContainer = document.querySelector(".line-container");
+  const menuContainer = document.querySelector(".menu-container");
+
+  lineContainer.addEventListener("click", () => {
+    lineContainer.classList.toggle("active");
+    menuContainer.classList.toggle("active");
+  });
+};
